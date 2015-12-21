4d1b680abfd537078985a1440190704142aa259d
casejoke <casejoke@gmail.com>
2015-12-21 13:16:23 +0300

    dev

--- admin/config-dev.php
+++ admin/config-dev.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://antibio.loc/admin/');
-define('HTTP_CATALOG', 'http://antibio.loc/');
+define('HTTP_SERVER', 'http://moyarmarka.ru/admin/');
+define('HTTP_CATALOG', 'http://moyarmarka.ru/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://antibio.loc/admin/');
-define('HTTPS_CATALOG', 'http://antibio.loc/');
+define('HTTPS_SERVER', 'http://moyarmarka.ru/admin/');
+define('HTTPS_CATALOG', 'http://moyarmarka.ru/');
 
 // DIR
-define('DIR_APPLICATION', '/xxx/admin/');
-define('DIR_SYSTEM', '/xxx/system/');
-define('DIR_LANGUAGE', '/xxx/admin/language/');
-define('DIR_TEMPLATE', '/xxx/admin/view/template/');
-define('DIR_CONFIG', '/xxx/system/config/');
-define('DIR_IMAGE', '/xxx/image/');
-define('DIR_CACHE', '/xxx/system/cache/');
-define('DIR_DOWNLOAD', '/xxx/system/download/');
-define('DIR_UPLOAD', '/xxx/system/upload/');
-define('DIR_LOGS', '/xxx/system/logs/');
-define('DIR_MODIFICATION', '/xxx/system/modification/');
-define('DIR_CATALOG', '/xxx/catalog/');
+define('DIR_APPLICATION', '/var/www/dev/data/www/moyarmarka.ru/admin/');
+define('DIR_SYSTEM', '/var/www/dev/data/www/moyarmarka.ru/system/');
+define('DIR_LANGUAGE', '/var/www/dev/data/www/moyarmarka.ru/admin/language/');
+define('DIR_TEMPLATE', '/var/www/dev/data/www/moyarmarka.ru/admin/view/template/');
+define('DIR_CONFIG', '/var/www/dev/data/www/moyarmarka.ru/system/config/');
+define('DIR_IMAGE', '/var/www/dev/data/www/moyarmarka.ru/image/');
+define('DIR_CACHE', '/var/www/dev/data/www/moyarmarka.ru/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/dev/data/www/moyarmarka.ru/system/download/');
+define('DIR_UPLOAD', '/var/www/dev/data/www/moyarmarka.ru/system/upload/');
+define('DIR_LOGS', '/var/www/dev/data/www/moyarmarka.ru/system/logs/');
+define('DIR_MODIFICATION', '/var/www/dev/data/www/moyarmarka.ru/system/modification/');
+define('DIR_CATALOG', '/var/www/dev/data/www/moyarmarka.ru/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', '');
+define('DB_USERNAME', 'casejoke');
+define('DB_PASSWORD', 'z9UmJGnN883Mnn43');
+define('DB_DATABASE', 'casejoke_yarmarka');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
